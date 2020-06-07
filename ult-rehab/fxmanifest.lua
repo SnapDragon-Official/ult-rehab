22987807a42f91f9d65dde6dff54538dc87e96ad
Snap <64895070+SnapDragon-cmd@users.noreply.github.com>
2020-06-07 14:55:09 +0300

    Add files via upload

---
+++ ult-rehab/fxmanifest.lua
@@ -0,0 +1,10 @@
+files {"interiorproxies.meta"}
+
+client_scripts{ "removepeds.lua"}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'adamant'
+games {'gta5'}
